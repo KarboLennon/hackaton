3ff59abaf64b9c7a692e261acbf35a2e317a280f
Muchtar Ali Anwar <muchtar.ali15@gmail.com>
2025-09-24 13:12:00 +0700

    api

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,12 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie',] ,
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*','http://localhost:4321','https://hackaton.muchtaralianwar.com'],
-
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://hackaton.muchtaralianwar.com')),
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
-
+    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
 ];
